acdb49dc4f13695b7147091077474d6dcc662b44
umut.dural <umut.dural@rwth-aachen.de>
2022-08-15 22:35:20 +0200

    Update lonpos.py
    Created a class for the solver

--- lonpos.py
+++ lonpos.py
@@ -1,5 +1,5 @@
 # -----------------------------------------------------------
-# Lonpos-solver : a brute-force solver for a puzzle called Lonpos
+# Solve-Lonpos : a brute-force solver for a physical puzzle called Lonpos
 #
 # MIT License
 #
@@ -27,98 +27,129 @@
 
 import numpy as np
 
-# We define the dictionary of blocks as set of vectors
-# For example:
-#----O  
-#---OOO     : (0,0),(1,0),(2,0),(1,1),(1,-1)
-#----O
-#
-# Idea: Use the leftmost point of a block as relative origin and fill the board from left to right. 
-# This way
-
-blocks_dict = {1:[(0,0),(0,1)]}
 
 
-board = np.zeros((5,11),dtype=int)
+class LonposSolver():
 
-print(board[(1,1)])
+    # We define the dictionary of blocks as set of vectors
+    # For example:
+    #----O  
+    #---OOO     : (0,0),(1,0),(2,0),(1,1),(1,-1)
+    #----O
+    #
+    # Idea: Use the leftmost point of a block as relative origin and fill the board from left to right. 
+    # This way
+
+    def __init__(self,blocks_dict,board_shape) -> None:
+
+        self.blocks_dict = blocks_dict
+        self.board = np.zeros(board_shape,dtype=int)
+
+        pass
 
 
-def rotate_block(block):
-    r_matrix = np.matrix([[0,-1],
+
+    def rotate_block(self,block):
+        r_matrix = np.matrix([[0,-1],
                           [1, 0]])
 
-    # The columns of this matrix are the relative points of the blocks
-    c_matrix= np.transpose(np.matrix(blocks_dict[block]))
+        # The columns of this matrix are the relative points of the blocks
+        c_matrix = np.transpose(np.matrix(self.blocks_dict[block]))
 
-    rotated = r_matrix @ c_matrix
+        rotated = r_matrix @ c_matrix
 
-    # We still need to shift the points 
-    # Find the minimum of each axis and push it so that the left-top corner is the new origin
-    leftmost = min(rotated[0])
-    rotated[0] -= min(rotated[0]) * np.ones(1,len(rotated[0]))
+        # We still need to shift the points 
+        # Find the minimum of each axis and push it so that the left-top corner is the new origin
+        leftmost = min(rotated[0])
+        rotated[0] -= min(rotated[0]) * np.ones(1,len(rotated[0]))
 
-    # Find the point with the maximum y which is in the leftmost column and shift accordingly so that this point is the (0,0) now
-    rotated[1] -= max([rotated[1][i] for i in range(len(rotated[0])) if rotated[0][i] == leftmost]) * np.ones(1,len(rotated[0]))
+        # Find the point with the maximum y which is in the leftmost column and shift accordingly so that this point is the (0,0) now
+        rotated[1] -= max([rotated[1][i] for i in range(len(rotated[0])) if rotated[0][i] == leftmost]) * np.ones(1,len(rotated[0]))
 
-    return rotated[:, 0:]
+        return rotated[:, 0:]
 
-def is_empty(board,position):
-### Checks if a position in the given board is empty and returns the boolean value
-    return board[position] == 0
+    def is_empty(self,board,position):
+        """ Checks if a position in the given board is empty and returns the boolean value
+        """
+        return board[position] == 0
 
 
-def place_block(board,block_name,block_points,position):
-    """ Places the block if it's possible in the given position, returns a new board """ 
+    def place_block(self,board,block_name,block_points,position):
+        """ Places the block if it's possible in the given position, returns a new board, if not possible returns False
 
-    new_board = board.copy()
+        :param board:
+        :type board:
+        :param block_name: 
+        :type block_name: 
+        :param block_points: the points of the block relative to the origin (pivot) of this block
+        :type block_points: list of tuples
+        :param position: a tuple of x and y coordinate in the board
+        :type position: tuple
+        :return: the new board with the block placed in the given position
+        :rtype: np.matrix | boolean
+        """ 
 
-    # Iterate through all indicex of a block
-    for point in block_points:
+        new_board = board.copy()
 
-        # Return the old board if it is not possible to place and also false to indicate that the operation was failed
-        if not is_empty(board,point + position):
-          return board,False
+        # Iterate through all indicex of a block
+        for point in block_points:
+
+            # Return the old board if it is not possible to place and also false to indicate that the operation was failed
+            if not self.is_empty(board,point + position):
+                return board,False
 
         new_board[point + position] = block_name
 
-
-    return new_board, True
-
+        return new_board, True
 
 
-def find_first_empty(board):
-    """ Find the first empty point in the given board. Iterates through rows and then columns for our heuristic 
+
+    def find_first_empty(self,board):
+        """ Find the first empty point in the given board. Iterates through rows and then columns for our heuristic 
     
-    """
-    shape = board.shape()
-    for column in range(shape[1])
-        for row in range(shape[0]):
-            position = (row,column)
-            if is_empty(board,position):
-                return position
-    return False
+        """
+        shape = board.shape()
+        for column in range(shape[1]):
+            for row in range(shape[0]):
+                position = (row,column)
+                if self.is_empty(board,position):
+                    return position
+        return False    
 
-def solve(board,block_names_set):
+    def solve(self,board,block_names_set):
+        """ Solve the given board with the given blocks recursively
+        """
 
-    first_empty = find_first_empty(board)
+        first_empty = self.find_first_empty(board)
 
-    if first_empty == False:
-        return board    
+        # If there isn't any empty point in the board return the solution
+        if first_empty == False:
+            return board    
 
-    for block_name in block_names_set:
-        rotated_block_points = blocks_dict[block_name]
-        for i in range(4):  
+        # For all remaining blocks in the set:
+        for block_name in block_names_set:
 
-            new_board, check = place_block(board,block_name,rotated_block_points)
+            #For all rotations of the block:
+            rotated_block_points = self.blocks_dict[block_name]
+            for i in range(4):  
+                # Try to place the block
+                new_board, check = self.place_block(board,block_name,rotated_block_points)
 
-            if check:
-                result = solve(new_board, block_names_set.difference(set(block_name)))
-                if result != False:
-                    return result 
+                # If the block is placed
+                if check:
+                    # Try to solve the new_board with the remaining blocks
+                    result = self.solve(new_board, block_names_set.difference(set(block_name)))
 
-            rotated_block_points = rotate_block(rotated_block_points)
-    return False
+                    # If it was possible to fill it return the result
+                    if result != False:
+                        return result 
+
+                    # If it is not possible with this combination, iterate through other rotations and blocks
+
+                # Rotate the block
+                rotated_block_points = self.rotate_block(rotated_block_points)
+
+        return False
 
 
 
